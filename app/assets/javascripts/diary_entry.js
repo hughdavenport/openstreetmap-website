97efe8dbc4d2899eb28be5a659cbdd550617d6d7
Matt Heard <matt@mattheard.net>
2016-04-05 10:50:24 +1200

    Wrap longitude selection when editing a diary entry
    This work was done while pairing with @hughdavenport
    
    Fixes #1191.
    
    As described by the bug report, if a user was editing a diary entry, and
    zoomed out on the map, and clicked outside the nominal range of -180 to
    180, then the longitude value was not being wrapped around to fit within
    that range.
    
    This patch adds a function to the javascript asset which wraps the value
    to fit within that range. As the range is open at both ends (-180 and
    180 are two separate values), an edge case was needed for that. The rest
    of the range is done with a custom wrap function which makes use of the
    mod function with an offset, and a custom sign function to keep the same
    sign as entered.

--- app/assets/javascripts/diary_entry.js
+++ app/assets/javascripts/diary_entry.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+  var LONGITUDE_MAX_VALUE = 180;
+  var LONGITUDE_RANGE_WIDTH = 360;
+
   var marker, map;
 
   function setLocation(e) {
     $("#latitude").val(e.latlng.lat);
-    $("#longitude").val(e.latlng.lng);
+    $("#longitude").val(wrapLongitude(e.latlng.lng));
 
     if (marker) {
       map.removeLayer(marker);
@@ -11,6 +14,34 @@ $(document).ready(function () {
 
     marker = L.marker(e.latlng, {icon: OSM.getUserIcon()}).addTo(map)
       .bindPopup(I18n.t('diary_entry.edit.marker_text'));
+  }
+
+  function wrapLongitude(longitude) {
+    if (longitude == LONGITUDE_MAX_VALUE) {
+      return LONGITUDE_MAX_VALUE;
+    } else {
+      return wrapAroundRange(longitude, LONGITUDE_RANGE_WIDTH);
+    }
+  }
+
+  /**
+   * Wraps a number to fit within a range centred around 0.
+   *
+   * Inclusive at lower end of range, exclusive at top end.
+   *
+   * x - Number to wrap
+   * width - Width of range
+   */
+  function wrapAroundRange(x, width) {
+    var offset = width / 2;
+
+    // If x is lower end of range, sign will return +1. (Math.sign would
+    // return 0, which would make the final result 0, which is incorrect.)
+    return sign(x + offset) * (Math.abs(x + offset) % width - offset);
+  }
+
+  function sign(x) {
+    return (x == 0 ? +1 : Math.sign(x));
   }
 
   $("#usemap").click(function (e) {
